4a3f22ba052f98ece6750defaf74d261d0688d1a
Deivison Gonçalves <97059867+Deivisongs@users.noreply.github.com>
2024-08-26 13:41:55 -0300

    Add files via upload

---
+++ pages/home/home.js
@@ -0,0 +1,47 @@
+/* ------ Ajustando Tamanho de tela --------*/
+function ajusteTamanhoTela(){
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+ajusteTamanhoTela()
+window.addEventListener('resize', ajusteTamanhoTela);
+
+
+
+/* ---------- Abir e Fechar Modal Notificações ------------*/
+
+function abrirNotificacoes(){
+    const modal = document.getElementById('modalNotificacoes')
+    const notificacoes = document.getElementById('notificacoes')
+    const body = document.getElementById('body')
+
+
+    if(notificacoes.style.display != 'block'){
+        notificacoes.style.display = 'block'
+        body.style.overflow = 'hidden'
+        modal.style.width = '100vw'
+    }else{
+        notificacoes.style.display = 'none'
+        body.style.overflow = 'auto'
+        modal.style.width = 'fit-content'
+    }
+
+
+
+}
+
+
+/* ------ Abrir e Fechar Modal Relatorio -------- */
+function abrirRelatorio(){
+    const modal = document.getElementById('modalRelatorio')
+    const body = document.getElementById('body')
+
+    if(modal.style.display != 'flex'){
+        modal.style.display = 'flex';
+        body.style.overflow = 'hidden';
+    }else{
+        modal.style.display = 'none';
+        body.style.overflow = 'auto';
+    }
+    
+}
